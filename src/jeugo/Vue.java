5e87e3f4d5441fd1c26865dbf27f7cd939f43b5c
Sacha Brisset <sacha.brisset@hotmail.fr>
2015-12-03 14:45:09 +0100

    Architecture de la vue

---
+++ src/jeugo/Vue.java
@@ -0,0 +1,23 @@
+package jeugo;
+
+/**
+ * Created by seti on 03/12/15.
+ */
+public class Vue {
+    public Vue() {
+    }
+    public void afficherPlateau(){
+
+    }
+    public int[] demanderPosition(){
+        int i[] = {0,0};
+        return i;
+    }
+    public void afficherInformation(String info){
+
+    }
+    public String[] demanderNomsJoueurs(){
+        String[] s = {};
+        return s;
+    }
+}
