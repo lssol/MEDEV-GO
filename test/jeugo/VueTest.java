ad6d59fed338fa7eb6916e998ac06cfd5f2eebb9
Solene Moreau <solene.moreau@eleves.ec-nantes.fr>
2015-12-10 14:40:46 +0100

    La classe VueTest ne fait rien, il faudrait utiliser un robot pour tester l'écriture au clavier à faire plus tard s'il y a le temps

---
+++ test/jeugo/VueTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeugo;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author solenemoreau
+ */
+public class VueTest {
+    
+    public VueTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of afficherPlateau method, of class Vue.
+     */
+    @org.junit.Test
+    public void testAfficherPlateau() {
+    }
+
+    /**
+     * Test of demanderPosition method, of class Vue.
+     */
+    @org.junit.Test
+    public void testDemanderPosition() {
+    }
+
+    /**
+     * Test of afficherInformation method, of class Vue.
+     */
+    @org.junit.Test
+    public void testAfficherInformation() {
+    }
+
+    /**
+     * Test of demanderNomsJoueurs method, of class Vue.
+     */
+    @org.junit.Test
+    public void testDemanderNomsJoueurs() {
+    }
+    
+}
