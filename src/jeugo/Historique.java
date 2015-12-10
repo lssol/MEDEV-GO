d77686de34a1e76c10b30c984507026329c3f9a8
Solene Moreau <solene.moreau@eleves.ec-nantes.fr>
2015-12-10 16:26:32 +0100

    Tests pour la classse Historique Correction de bugs dans la classe Historique

--- src/jeugo/Historique.java
+++ src/jeugo/Historique.java
@@ -24,27 +24,28 @@ public class Historique extends HashMap<Integer,ArrayList<Piece[][]>>{
     /**
      * Compare deux matrices de pièces
      * Renvoie 1 si elles sont identiques
-     * Renvoie -1 si elles sont non comparables (de tailles différentes)
+     * Renvoie -1 si elles sont non comparables (de tailles différentes ou vides)
      * Renvoie 0 si elles sont comparables mais différentes
-     * @param p1 une matrice de pièces
-     * @param p2 une matrice de pièces
+     * @param mat1 une matrice de pièces
+     * @param mat2 une matrice de pièces
      * @return un entier
      */
-    public int compareMat(Piece[][] p1, Piece[][] p2) {
-        if (p1.length != p2.length) {
+    public int compareMat(Piece[][] mat1, Piece[][] mat2) {
+        if (mat1.length == 0 || mat2.length == 0 || mat1.length != mat2.length) {
             return -1;
         } else {
-            if (p1[1].length != p2[1].length) {
+            if (mat1[1].length != mat2[1].length) {
                 return -1;
             } else {
                 int m = 0;
                 int n = 0;
-                int mm = p2.length;
-                int nn = p2[1].length;
+                int mm = mat2.length;
+                int nn = mat2[1].length;
                 boolean egal = true; // passera à false si on tombe sur 2 pièces différentes
                 boolean finParcours = false; // passera à true lorsque toute la matrice aura été parcourue
                 while (egal && !finParcours) {
-                    if (p1[m][n].equals(p2[m][n])) {
+                    if (mat1[m][n] == null && mat2[m][n] == null) {
+                        // les 2 matrices sont vides pour la case [m][n]
                         // mise à jour des indices de parcours m et n
                         if (n<nn-1) { // est-on sur la dernière colonne ?
                             n++;
@@ -57,7 +58,27 @@ public class Historique extends HashMap<Integer,ArrayList<Piece[][]>>{
                             }
                         }
                     } else {
-                        egal = false;
+                        if (mat1[m][n] == null || mat2[m][n] == null) {
+                            // la case [m][n] est vide pour 1 des deux matrices (et une seule)
+                            egal = false;
+                        } else {
+                            // la case [m][n] n'est vide dans aucune des 2 matrices
+                            if (mat1[m][n].compareCouleurPosition(mat2[m][n])) {
+                                // mise à jour des indices de parcours m et n
+                                if (n<nn-1) { // est-on sur la dernière colonne ?
+                                    n++;
+                                } else {
+                                    if (m<mm-1) { // est-on sur la dernière ligne ?
+                                        n=0;
+                                        m++;
+                                    } else {
+                                        finParcours = true;
+                                    }
+                                }
+                            } else {
+                                egal = false;
+                            }
+                        }
                     }
                 }
                 if (!egal) {
@@ -73,15 +94,15 @@ public class Historique extends HashMap<Integer,ArrayList<Piece[][]>>{
     
     /**
      * Teste si une configuration des pièces a déjà existé plus tôt dans le jeu
-     * @param p une matrice de pièces (une configurations de pièces)
+     * @param mat une matrice de pièces (une configurations de pièces)
      * @return un booléen
      */
-    public boolean existe(Piece[][] p) {
-        // on calcule le nombre de pièces dans la matrice p
+    public boolean existe(Piece[][] mat) {
+        // on calcule le nombre de pièces dans la matrice mat
         int nbPieces = 0;
-        for (int i = 0; i < p.length; i++) {
-            for (int j = 0; j < p[1].length; i++) {
-                if (p[i][j] != null) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[1].length; j++) {
+                if (mat[i][j] != null) {
                     nbPieces++;
                 }
             }    
@@ -92,13 +113,21 @@ public class Historique extends HashMap<Integer,ArrayList<Piece[][]>>{
             // il existe une configuration avec le même nombre de pièces
             ArrayList<Piece[][]> listeMatPieces = this.get(nbPieces);
             boolean trouve = false; 
-            // trouve passera à vrai lorsqu'une configuration similaire sera trouvée
+            // trouve passera à true lorsqu'une configuration similaire sera trouvée
+            boolean finParcours = false;
+            // finParcours passera à true lorsque toute la liste aura été parcourue
             int indListe = 0;
-            while (!trouve && listeMatPieces.get(indListe) != null) {
-                indListe++;
-                if (compareMat(p,listeMatPieces.get(indListe)) == 1) {
+            while (!trouve && !finParcours && listeMatPieces.get(indListe) != null) {
+                if (compareMat(mat,listeMatPieces.get(indListe)) == 1) {
                     // on a trouvé une configuration similaire
                     trouve = true;
+                }
+                // mise à jour de indListe
+                if (indListe < listeMatPieces.size()-1) {
+                    indListe++;
+                } else {
+                    // on a fini de parcourir la liste
+                    finParcours = true;
                 }
             }
             return trouve;
---
+++ test/jeugo/HistoriqueTest.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeugo;
+
+import java.util.ArrayList;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author solenemoreau
+ */
+public class HistoriqueTest {
+    
+    public HistoriqueTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of compareMat method, of class Historique.
+     */
+    @Test
+    public void testCompareMat() {
+        Historique h = new Historique();
+        
+        // 2 matrices de pièces vides
+        Piece[][] mat1 = new Piece[0][0];
+        Piece[][] mat2 = new Piece[0][0];
+        assertEquals(-1,h.compareMat(mat1,mat2));
+   
+        // 2 matrices de pièces de tailles différentes
+        mat1 = new Piece[16][16];
+        mat2 = new Piece[9][9];
+        assertEquals(-1,h.compareMat(mat1,mat2));
+        
+        // 2 matrices de pièces de même taille et identiques
+        mat1 = new Piece[16][16];
+        mat2 = new Piece[16][16];
+        Position pos1 = new Position(3,4);
+        Piece p1 = new Piece(true,pos1);
+        mat1[3][4] = p1;
+        Piece p2 = new Piece(true,pos1);
+        mat2[3][4] = p2;
+        assertEquals(1,h.compareMat(mat1,mat2));
+        
+        // 2 matrices de pièces de même taille mais différentes (2 pièces, pas au même endroit)
+        mat1 = new Piece[16][16];
+        mat2 = new Piece[16][16];
+        pos1 = new Position(1,1);
+        p1 = new Piece(true,pos1);
+        mat1[1][1] = p1;
+        Position pos2 = new Position(6,3);
+        p2 = new Piece(true,pos2);
+        mat2[6][3] = p2;
+        assertEquals(0,h.compareMat(mat1,mat2));
+        
+        // 2 matrices de pièces de même taille mais différentes (2 pièces au même endroit de couleur différente)
+        mat1 = new Piece[16][16];
+        mat2 = new Piece[16][16];
+        pos1 = new Position(10,4);
+        p1 = new Piece(true,pos1);
+        mat1[10][4] = p1;
+        p2 = new Piece(false,pos1);
+        mat2[10][4] = p2;
+        assertEquals(0,h.compareMat(mat1,mat2));
+    }
+    
+    /**
+     * Test of existe method, of class Historique.
+     */
+    @Test
+    public void testExiste() {
+        
+        // configuration 1 : true en (3,4)
+        Piece[][] mat1 = new Piece[16][16];
+        Position pos1 = new Position(3,4);
+        Piece p1 = new Piece(true,pos1);
+        mat1[3][4] = p1;
+        
+        // configuration 2 : false en (3,4)
+        Piece[][] mat2 = new Piece[16][16];
+        Position pos2 = new Position(3,4);
+        Piece p2 = new Piece(true,pos2);
+        mat2[3][4] = p2;
+        
+        // configuration 3 : true en (3,4) et false en (9,7)
+        Piece[][] mat3 = new Piece[16][16];
+        Position pos3a = new Position(3,4);
+        Position pos3b = new Position(9,7);
+        Piece p3a = new Piece(true,pos3a);
+        Piece p3b = new Piece(false,pos3b);
+        mat3[3][4] = p3a;
+        mat3[9][7] = p3b;
+        
+        // configuration 4 : true en (3,4) et true en (9,7)
+        Piece[][] mat4 = new Piece[16][16];
+        Position pos4a = new Position(3,4);
+        Position pos4b = new Position(9,7);
+        Piece p4a = new Piece(true,pos4a);
+        Piece p4b = new Piece(true,pos4b);
+        mat4[3][4] = p4a;
+        mat4[9][7] = p4b;
+
+        // historique avec les configurations 1, 2, 3 et 4
+        Historique h1234 = new Historique();
+        ArrayList<Piece[][]> l1 = new ArrayList<>();
+        l1.add(mat1);
+        l1.add(mat2);
+        ArrayList<Piece[][]> l2 = new ArrayList<>();
+        l2.add(mat3);
+        l2.add(mat4);
+        h1234.put(1,l1);
+        h1234.put(2,l2);
+        
+        // historique avec les configurations 1, 2 et 3
+        Historique h123 = new Historique();
+        ArrayList<Piece[][]> l3 = new ArrayList<>();
+        l3.add(mat1);
+        l3.add(mat2);
+        ArrayList<Piece[][]> l4 = new ArrayList<>();
+        l4.add(mat3);
+        h123.put(1,l3);
+        h123.put(2,l4);
+        
+        // tests
+        assertTrue(h1234.existe(mat1));
+        assertTrue(h1234.existe(mat2));
+        assertTrue(h1234.existe(mat3));
+        assertTrue(h1234.existe(mat4));
+        assertTrue(h123.existe(mat1));
+        assertTrue(h123.existe(mat2));
+        assertTrue(h123.existe(mat3));
+        assertFalse(h123.existe(mat4));
+    }
+
+    
+    
+}
